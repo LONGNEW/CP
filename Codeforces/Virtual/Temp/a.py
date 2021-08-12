cbf9fcf6d8eee481f1205a454c27c04f7a85a5d7
ShinHyoung <jsin2475@naver.com>
2021-08-12 22:32:08 +0900

    2021.08.12

--- Codeforces/Virtual/Temp/a.py
+++ Codeforces/Virtual/Temp/a.py
@@ -1,11 +1,5 @@
-import sys
+ans = 0
+for i in range(1, 21):
+    ans += i
 
-for _ in range(int(sys.stdin.readline())):
-    n = int(sys.stdin.readline())
-    data = list(map(int, sys.stdin.readline().split()))
-    data.sort()
-    small, big = data[:n + 1], data[n:]
-
-    for i in range(n):
-        print(small[i], big[i], end=" ")
-    print()
+print(ans)
